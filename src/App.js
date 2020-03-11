f44ecfb34901ff1d0b75e61f9bc32d854ce1b446
Daniel Fong <daniel.fong.mv@gmail.com>
2020-03-10 23:22:24 -0600

    started delete handler

--- src/App.js
+++ src/App.js
@@ -5,7 +5,45 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import LoginPage from './routes/LoginPage/LoginPage'
 import RegistrationPage from './routes/RegistrationPage/RegistrationPage'
 
+import snapshotContext from './context/snapshotContext'
+
 class App extends React.Component {
+  state = {
+    games: [],
+    players: [],
+    error: null
+  };
+
+  fetchGamesByUserId(userId) {
+    return(
+      fetch(`http://localhost:8080/api/games/${userId}`)
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject('Error fetching notes from server');
+        })
+        .catch(err => {
+          this.setState({error: err})
+        })
+      )
+    }
+
+  fetchPlayersByGameId(gameId) {
+    return(
+      fetch(`http://localhost:8080/api/players/${gameId}`)
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject('Error fetching players from server')
+        })
+        .catch(err => {
+          this.setState({error: err})
+        })
+    )
+  } 
+
   render() {
     return (
       <div className="App">
