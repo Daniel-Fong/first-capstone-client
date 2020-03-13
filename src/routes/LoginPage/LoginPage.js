d160ed9830752dd0909189e6723f01d773955841
Daniel Fong <daniel.fong.mv@gmail.com>
2020-03-13 08:58:11 -0600

    added login form

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import LoginForm from '../../components/LoginForm/LoginForm'
 
 class LoginPage extends React.Component {
+    static defaultProps = {
+        location: {},
+        history: {
+            push: () => {},
+        },
+    }
+
+    handleLoginSuccess = () => {
+        const { location, history } = this.props
+        const destination = (location.state || {}).from || '/'
+        history.push(destination)
+    }
+
     render() {
         return (
             <div className='login-page-div'>
                 <h2>Existing Users</h2>
                 <h3>Sign In</h3>
-                <form>
-                    <label for='sign-in-username'>Username</label>
-                    <input name='sign-in-username' />
-                    <label for='sign-in-password'>Password</label>
-                    <input name='sign-in-password' />
-                    <button>Submit</button>
-                </form>
+                <LoginForm onLoginSuccess={this.handleLoginSuccess} />
                 <Link to='/'>
                     <button>Cancel</button>
                 </Link>
